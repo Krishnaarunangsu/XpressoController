8d5f7bf94982964c75e6177f0393e8f3b02e474b
root <naveen.sinha@abzooba.com>
2020-02-04 09:24:35 +0000

    OK

--- controller_1.2.16/python_process_1.py
+++ controller_1.2.16/python_process_1.py
@@ -3,6 +3,6 @@ import subprocess
 # ps1 = subprocess.run(['ls'], universal_newlines=True, stdout=subprocess.PIPE)
 # ps2 = subprocess.run(['pwd'], universal_newlines=True, input=ps1.stdout)
 
-ps1 = subprocess.run(['xprctl -h'], universal_newlines=True, stdout=subprocess.PIPE)
-ps2 = subprocess.run(['xprctl list'], universal_newlines=True, input=ps1.stdout)
-ps3 = subprocess.run(['xprctl info'], universal_newlines=True, input=ps2.stdout)
+ps1 = subprocess.Popen(['xprctl login -u ASahu2 -w qa'], universal_newlines=True, stdout=subprocess.PIPE)
+# ps2 = subprocess.Popen(['pwd'], universal_newlines=True, input=ps1.stdout)
+# ps3 = subprocess.Popen(['xprctl info'], universal_newlines=True, input=ps2.stdout)
